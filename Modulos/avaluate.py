18ead02a838ec5e9818c53733acd354cce8cb97f
ViitoDev <joaovictorpiresdev@gmail.com>
2025-12-01 21:01:31 -0300

    feat - adding evaluating libraries

--- Modulos/avaluate.py
+++ Modulos/avaluate.py
@@ -2,5 +2,3 @@ class Evaluating:
     def __init__(self, client, note):
         self._client = client
         self._note = note
-
-        
--- Modulos/libraries.py
+++ Modulos/libraries.py
@@ -14,17 +14,28 @@ class Library:
     
     @classmethod
     def list_Libraries(cls):
-        print(f"{'Nome da biblioteca'.ljust(25)} | {'Status'}")
+        print(f"{'Nome da biblioteca'.ljust(25)}| {'Nota média'.ljust(25)} | {'Status'}")
         for library in Library.libraries:
-            print(f"{library.name.ljust(25)} | {library.active}")
+            print(f"{library.name.ljust(25)}| {str(library.evaluate_media).ljust(25)} | {library.active}")
 
     def toggle_state(self):
         self._active = not self._active
 
     @property
     def active(self):
-        return "Ativada" if self._active else "Desativada"
+        if self._active and self.evaluation:
+            return "Ativada"
+        else:
+            return "Desativada"
 
     def receive_evaluation(self, client, note):
         evaluate = Evaluating(client, note)
         self.evaluation.append(evaluate)
+
+    @property
+    def evaluate_media(self):
+        if not self.evaluation:
+            return "-"
+        sum1 = sum(evaluate._note for evaluate in self.evaluation)
+        average = round(sum1 / len(self.evaluation))
+        return average
